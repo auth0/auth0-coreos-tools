eba98169432109a6fba24c4f61fbd4c2a0a3271e
Tomasz Janczuk <tomasz@janczuk.org>
2014-10-07 13:17:45 -0700

    support for capitalizing property names from etcd

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,21 @@
 if (typeof process.argv[2] !== 'string') 
-    throw new Error('Usage: etcd_get {key}');
+    throw new Error('Usage: etcd_get {key} [--toupper]');
+if (typeof process.argv[3] !== undefined && process.argv[3] !== '--toupper')
+    throw new Error('Unrecognized option: ' + process.argv[3] + '. Usage: etcd_get {key} [--toupper]');
 
 require('../lib/index').etcd_get(process.argv[2], function (error, result) {
     if (error)
         throw error;
+
+    if (process.argv[3] === '--toupper') 
+        result = upper(result);
     console.log(JSON.stringify(result, null, 2));
 });
+
+// Convert object property names to uppercase
+function upper(o) {
+    var r = {};
+    for (var k in o) 
+        r[k.toUpperCase()] = typeof o[k] === 'object' ? upper(o[k]) : o[k];
+    return r;
+}
