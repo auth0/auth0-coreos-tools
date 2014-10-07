c053cf3afae71318db94f0e1046494c6ff0b0125
Tomasz Janczuk <tomasz@janczuk.org>
2014-10-07 13:52:04 -0700

    remove toml

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,12 @@
 if (typeof process.argv[2] !== 'string') 
-    throw new Error('Usage: etcd_get {key} [--upper] [--toml]');
+    throw new Error('Usage: etcd_get {key} [--upper]');
 
-var toml, toupper, z = 3;
+var toupper, z = 3;
 while (process.argv[z] !== undefined) {
     if (process.argv[z] === '--upper') toupper = true;
-    else if (process.argv[z] === '--toml') toml = true;
     else
         throw new Error('Unrecognized option: ' + process.argv[z] 
-            + '. Usage: etcd_get {key} [--upper] [--toml]');
+            + '. Usage: etcd_get {key} [--upper]');
     z++;
 }
 
@@ -17,27 +16,8 @@ require('../lib/index').etcd_get(process.argv[2], function (error, result) {
 
     if (toupper) 
         result = upper(result);
-    if (toml) 
-        console.log(toml(result));
-    else 
-        console.log(JSON.stringify(result, null, 2));
+    console.log(JSON.stringify(result, null, 2));
 });
-
-// Serialize JSON to TOML; up to 1 hierarchy level is supported
-function toml(o) {
-    var result = '';
-    var sections = [];
-    for (var k in o) {
-        if (typeof o[k] === 'object')
-            sections.push(k);
-        else
-            result += k + ' = ' + o[k] + '\n';
-    }
-    sections.forEach(function (k) {
-        result += '[' + k ']\n' + toml(o[k]);
-    });
-    return result;
-}
 
 // Convert object property names to uppercase
 function upper(o) {
